e100593e559ce5eba117c13b9bb0ea1c7f675d59
Randolph Wong <wwk.randolph@gmail.com>
2015-07-12 23:18:21 +0700

    cleaned up

--- FlogoMachine.ino
+++ FlogoMachine.ino
@@ -12,72 +12,61 @@
 #define ST_CP_low() ST_CP_PORT &= ~_BV(ST_CP_PIN - 8)
 #define DS_high() DS_PORT |= _BV(DS_PIN - 8)
 #define DS_low() DS_PORT &= ~_BV(DS_PIN - 8)
-#define test(bits) (1 << bits)
 
-#define delay_val 3
+//rotate left and rotate right
+#define rol(val, bits) ((val << 1) | (val >> (bits - 1))) & ((1 << bits) - 1)
+#define ror(val, bits) ((val >> 1) | (val << (bits - 1))) & ((1 << bits) - 1)
+
+#define DELAY_VAL 3
+#define REGISTER_COUNT 5
+#define FULL_TURN_STEPS 2047
 
 void setup() {
-  Serial.begin(9600);
   SPI.begin();
   
   pinMode(SH_CP_PIN, OUTPUT);
   pinMode(ST_CP_PIN, OUTPUT);
   pinMode(DS_PIN, OUTPUT);
   
-//  customShift(0, 8);
-//  customShift(0, 8);
-//  customShift(B10101010, 8);
-  
-  //byte val = 16;
-  byte val = _BV(0) + _BV(4);
-  for (int i = 0; i != 2047; ++i) {
-    ST_CP_low();
-    
-    for (int j = 0; j != 5; ++j)
-      SPI.transfer(val);
-    ST_CP_high();
-    //val = (val << 1 | val >> 3) & B11110000;
-    val = (val << 1 | val >> 7);
-    delay(delay_val);
-  }
-  
-  for (int i = 0; i != 5; ++i)
-    SPI.transfer(0);
-  ST_CP_low();
-  ST_CP_high();
-  
-  val = _BV(0) + _BV(4);
-  for (int i = 0; i != 2047; ++i) {
-    ST_CP_low();
-    
-    for (int j = 0; j != 5; ++j)
-      SPI.transfer(val);
-    ST_CP_high();
-    //val = (val << 1 | val >> 3) & B11110000;
-    val = (val >> 1 | val << 7);
-    delay(delay_val);
-  }
-  
-  for (int i = 0; i != 5; ++i)
-    SPI.transfer(0);
-  ST_CP_low();
-  ST_CP_high();
+  clearRegister(REGISTER_COUNT); 
+
+  testAllStepper();  
+
+  clearRegister(REGISTER_COUNT); 
 }
 
 void loop() {}
 
 void customShift(const unsigned long& val, const int& bits) {
-//  Serial.print("val: ");
-//  Serial.println(val);
   for (int i = 0; i < bits; ++i) {
     if ((_BV(i) & val) == _BV(i)) {
-      //Serial.print(1);
       DS_high();
     } else {
-      //Serial.print(0);
       DS_low();
     }
     SH_CP_high();
     SH_CP_low();    
+  }
+}
+
+void clearRegister(const int& register_count) {
+  for (int i = 0; i != register_count; ++i)
+    SPI.transfer(0);
+  ST_CP_low();
+  ST_CP_high();
+}
+
+void testAllStepper() {
+  byte val = B00010001; 
+  for (int i = 0; i != FULL_TURN_STEPS; ++i) {
+    ST_CP_low();
+    
+    for (int j = 0; j != REGISTER_COUNT; ++j)
+      SPI.transfer(val);
+    ST_CP_high();
+
+    //val = rol(val, 4); 
+    val = rol(val, 8); 
+    delay(DELAY_VAL);
   }
 }
