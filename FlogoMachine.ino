a42130c251bccf33ef54b135cdd1a9f3d9ed8e16
Randolph Wong <wwk.randolph@gmail.com>
2015-07-14 14:34:19 +0700

    implemented Shutter::resetShutter

--- FlogoMachine.ino
+++ FlogoMachine.ino
@@ -14,6 +14,8 @@
 #define DELAY_VAL 5
 #define GEAR_VAL 5
 #define REGISTER_COUNT 25
+#define STEPS_FOR_COMPLETE_SHUT1 60
+#define STEPS_FOR_COMPLETE_SHUT2 65
 
 class Motor {
 public:
@@ -38,12 +40,14 @@ public:
 class Shutter {
 public:
     Shutter(const int);
+    void resetSteps();
     void populateRegister();
     void populateRegister(const int, const char*);
+    void populateRegisterForReset();
     int getRemainingSteps();
     void beginShutter();
     void closeShutter();
-    void ResetShutter();
+    void resetShutter();
     int getRegisterValue(const int);
     void updateRegisterValue(const int);
 
@@ -59,6 +63,13 @@ Shutter::Shutter(const int reg_count) {
     reg = new Register[reg_count];
 }
 
+void Shutter::resetSteps() {
+    for (int i = 0; i != register_count; ++i) {
+        reg[i].motor1.steps_taken = 0;
+        reg[i].motor2.steps_taken = 0;
+    }
+}
+
 void Shutter::populateRegister() {
     char msg_buffer[11]; 
     for (int i = 0; i != register_count; ++i) {
@@ -72,11 +83,16 @@ void Shutter::populateRegister() {
 
 void Shutter::populateRegister(const int register_index, const char* serial_msg) {
     String reg_info = String(serial_msg);
-    reg[register_index].motor1.steps_taken = 0;
-    reg[register_index].motor2.steps_taken = 0;
     reg[register_index].motor1.target_steps = reg_info.substring(5, 7).toInt() * GEAR_VAL;
     reg[register_index].motor2.target_steps = reg_info.substring(8, 10).toInt() * GEAR_VAL;
 }
+
+//void Shutter::populateRegisterForReset() {
+//    for (int i = 0; i != register_count; ++i) {
+//        reg[i].motor1.target_steps = STEPS_FOR_COMPLETE_SHUT1 - reg[i].motor1.target_steps;
+//        reg[i].motor2.target_steps = STEPS_FOR_COMPLETE_SHUT2 - reg[i].motor2.target_steps;
+//    }
+//}
 
 int Shutter::getRemainingSteps() {
     int steps = 0;
@@ -88,10 +104,11 @@ int Shutter::getRemainingSteps() {
 }
 
 void Shutter::beginShutter() {
+    resetSteps();
     int remaining_steps = getRemainingSteps();
-    Serial.print("S");
-    Serial.print(remaining_steps);
-    Serial.print("E");
+//    Serial.print("S");
+//    Serial.print(remaining_steps);
+//    Serial.print("E");
     for (int i = 0; i != register_count; ++i) {
         reg[i].motor1.dir = 1;
         reg[i].motor1.val = 1;
@@ -104,14 +121,39 @@ void Shutter::beginShutter() {
         ST_CP_low();
         for (int i = 0; i != register_count; ++i) {
             SPI.transfer(getRegisterValue(i));
-            Serial.print(getRegisterValue(i)==B10001000);
+            //Serial.print(getRegisterValue(i)==B10001000);
             updateRegisterValue(i);
             delay(DELAY_VAL);
         ST_CP_high();
         }
         remaining_steps = getRemainingSteps();
-        Serial.print("s");
-        Serial.print(remaining_steps);
+        //Serial.print("s");
+        //Serial.print(remaining_steps);
+    }
+}
+
+void Shutter::resetShutter() {
+    resetSteps();
+    int remaining_steps = getRemainingSteps();
+    for (int i = 0; i != register_count; ++i) {
+        reg[i].motor1.dir = -1;
+        reg[i].motor1.val = 1;
+
+        reg[i].motor2.dir = -1;
+        reg[i].motor2.val = 1;
+    }
+     while (remaining_steps > 0) {
+        ST_CP_low();
+        for (int i = 0; i != register_count; ++i) {
+            SPI.transfer(getRegisterValue(i));
+            //Serial.print(getRegisterValue(i)==B10001000);
+            updateRegisterValue(i);
+            delay(DELAY_VAL);
+        ST_CP_high();
+        }
+        remaining_steps = getRemainingSteps();
+        //Serial.print("s");
+        //Serial.print(remaining_steps);
     }
 }
 
@@ -153,7 +195,7 @@ void loop() {
             shutter.populateRegister();
             shutter.beginShutter();
             //shutter.closeShutter();
-            //shutter.resetShutter();
+            shutter.resetShutter();
             Serial.print("done");
         }
     }
