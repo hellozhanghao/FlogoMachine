a4e5db736b732131c08cfdbe4d8e496645f7a84b
Randolph Wong <wwk.randolph@gmail.com>
2015-07-24 23:57:49 +0700

    Check if only have a single filled surface

--- flogoGUI.py
+++ flogoGUI.py
@@ -157,6 +157,7 @@ class App:
         else:
             self.surface.fillSurface()
             for filled_grid in self.surface.getFirstFilledSurface(): print "filled grid:", filled_grid
+            print "only one filled surface:", self.surface.hasOnlyOneFilledSurface()
             grids = self.grid_map.grids
             if not self.surface.isHorizontallyConvexedSurface():
                 # transpose matrix
